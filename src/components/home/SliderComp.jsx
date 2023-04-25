b5d253e822c20039020e71746c12d57eb45326ab
Helin Akdoğan <akdoganhelin@gmail.com>
2023-04-25 04:49:15 +0300

    25.04.2023

--- src/components/home/SliderComp.jsx
+++ src/components/home/SliderComp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from 'react-slick'
 
 const SliderComp = () => {
-    
+
     const settings = {
         dots: true,
         infinite: true,
@@ -14,13 +14,21 @@ const SliderComp = () => {
     return (
         <div>
             <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
+
+                <div className='!flex items-center bg-gray-100 px-6'>
+
+                    <div className=''>
+                        <div className='text-6xl font-bold'>Nike Sneakers</div>
+                        <div className='text-lg my-4'>With its easygoing lines, heritage track look and of course, visible Nike Air cushioning, the Air Max SC SE is the perfect finish to any outfit. The rich mixture of materials adds depth while making it a durable and lightweight shoe for everyday wear.</div>
+                        <div className='border rounded-full cursor-pointer text-2xl w-[200px] h-16 flex items-center justify-center bg-gray-200'>Go Details</div>
+                    </div>
+
+                    <img src='https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/ba390be5-76c3-44bd-9635-b56feb8b126c/air-max-sc-se-womens-shoes-CwMCK7.png' alt='' width='400px' height='400px' />
                 </div>
                 <div>
-                    <h3>2</h3>
+                    <img src='https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f820e398-3050-438b-91a9-e985898def17/spark-womens-shoes-w4WdlF.png' alt='' width='400px' height='400px' />
                 </div>
-                </Slider>
+            </Slider>
         </div>
     )
 }
