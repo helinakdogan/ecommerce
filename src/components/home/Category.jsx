7e772c972bf0083727accbc57c8cb415213fbe4d
Helin Akdoğan <akdoganhelin@gmail.com>
2023-04-27 03:33:08 +0300

    27.04.2023

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCategories } from '../../redux/categorySlice';
 
 // It will get categories by api and store.js is needed with redux
 
 const Category = () => {
+
+  const dispatch = useDispatch();
+  const { categories } = useSelector(state => state.categories);
+
+  console.log(categories, "categories");
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch])
+
+
   return (
-    <div className='w-1/6 bg-gray-100'>Category</div>
+    <div className='w-1/6 bg-gray-100 p-4'>
+      <div className='border-b pb-1 px-2 text-xl font-bold'>CATEGORY</div>
+      {
+        categories?.map((category, i) => (
+          <div className='text-lg m1-1 cursor-pointer hover:bg-gray-200 p-2' key={i}>{category}</div>
+        ))
+      }
+    </div>
   )
 }
 
