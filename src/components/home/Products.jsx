1d991474353a8a7ceba2b88a5fcb61f71a37c76f
Helin Akdoğan <akdoganhelin@gmail.com>
2023-05-02 23:48:56 +0300

    02.05.2023

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,34 +1,76 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../../redux/productSlice';
 import Loading from '../Loading';
 import Product from './Product';
+import ReactPaginate from 'react-paginate';
 
 
 const Products = () => {
 
   const dispatch = useDispatch();
   // These two come from productSlice.js
-  const {products, productsStatus} = useSelector(state => state.products);
+  const { products, productsStatus } = useSelector(state => state.products);
 
   console.log(products, "products");
 
-  useEffect (() => {
-      dispatch(getProducts())
+
+  // -------------------------- This is for pagination -------------------------------------
+
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const itemsPerPage = 6;
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = products.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(products.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  // ------------------------------------------------------------------------------------------
+
+
+
+  useEffect(() => {
+    dispatch(getProducts())
   }, [dispatch])
 
   return (
     <div>
       {
-        productsStatus == "LOADING" ? <Loading/> :
-        <div className='flex flex-wrap'>
-          {
-            products?.map((product, i) => (
-              <Product key={i} product={product}/>
-            ))
-          }
-        </div>
+        productsStatus == "LOADING" ? <Loading /> :
+          <>
+            <div className='flex flex-wrap'>
+              {
+                currentItems?.map((product, i) => (
+                  <Product key={i} product={product} />
+                ))
+              }
+            </div>
 
+            <ReactPaginate
+              className='paginate'
+              breakLabel="..."
+              nextLabel=">"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="<"
+              renderOnZeroPageCount={null}
+            />
+
+
+          </>
       }
     </div>
   )
