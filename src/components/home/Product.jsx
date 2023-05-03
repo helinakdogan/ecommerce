248a681f996f804f37c4ca690912418ec221a803
Helin Akdoğan <akdoganhelin@gmail.com>
2023-05-04 01:46:29 +0300

    04.05.2023

--- src/components/home/Product.jsx
+++ src/components/home/Product.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Product = ({product}) => {
+
+  const navigate = useNavigate();
+
   return (
-    <div className='w-[402px] p-3 mx-2 mb-2 border rounded-md relative cursor-pointer'>
+    <div onClick={() => navigate(`products/${product?.id}`)} className='w-[402px] p-3 mx-2 mb-2 border rounded-md relative cursor-pointer'>
 
       <div className='text-2xl font-bold absolute rounded-md top-0 right-0 bg-emerald-600 text-white p-2 m-1'> {product?.price} <span className='text-sm'>$</span> </div>
       <img className='w-[200px] h-[200px] object-cover m-auto' src={product?.image} alt=''/>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { getDetailProduct } from '../redux/productSlice';
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
+
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const { productDetail, productDetailStatus } = useSelector(state => state.products)
+
+    useEffect(() => {
+        dispatch(getDetailProduct(id));
+    }, [dispatch, id])
+
+    console.log(productDetail, "productDetail");
+    
+    return (
+        <div>Detail</div>
+    )
 }
 
 export default Detail
